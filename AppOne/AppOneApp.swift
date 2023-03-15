a92837f565dc3d02221492b0138fa946445320b0
ouardi <ouardi@MBP-de-ouardi.Home>
2023-03-15 10:43:56 +0100

    first commit

---
+++ AppOne/AppOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppOneApp.swift
+//  AppOne
+//
+//  Created by ouardi on 14/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
